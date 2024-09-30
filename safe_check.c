f0de355622594f026c7970d25d1d909007ef4070
rossnotfromfrients <rdolgopolovv@gmail.com>
2024-09-30 15:19:12 +0200

    Update safe_check.c

--- safe_check.c
+++ safe_check.c
@@ -10,18 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "include.h"
+#include "include.h" // Include the header file
 
-int	is_safe(int grid[N][N], int row, int col, int num)
+int	is_safe(int grid[N][N], int row, int col, int num) // Function to check if it's safe to insert a number
 {
-	int	x;
+	int	x; // Index for iterating through the row and column
 
-	x = 0;
-	while (x < N)
+	x = 0; // Initialize the index
+	while (x < N) // Loop through the elements of the row
 	{
-		if (grid[row][x] == num || grid[x][col] == num)
-			return (0);
-		x++;
+		if (grid[row][x] == num || grid[x][col] == num) // Check if the number already exists in the row or column
+			return (0); // Return 0 if the number is present
+		x++; // Move to the next element
 	}
-	return (1);
+	return (1); // Return 1 if the insertion is safe
 }

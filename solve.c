71a21290030947be37b97e78f52677fa997fa7bf
rossnotfromfrients <rdolgopolovv@gmail.com>
2024-09-30 15:19:51 +0200

    Update solve.c

--- solve.c
+++ solve.c
@@ -10,50 +10,50 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "include.h"
+#include "include.h" // Include the header file
 
-int	solve_recursive(int grid[N][N], t_clues clues, int row, int col)
+int	solve_recursive(int grid[N][N], t_clues clues, int row, int col) // Recursive function to solve the puzzle
 {
-	int	num;
+	int	num; // Variable for the current number
 
-	if (row == N)
-		return (is_valid(grid, clues));
-	if (col == N)
-		return (solve_recursive(grid, clues, row + 1, 0));
-	num = 1;
-	while (num <= N)
+	if (row == N) // Check if we reached the end of the grid
+		return (is_valid(grid, clues)); // If yes, check the validity of the current grid
+	if (col == N) // If reached the end of the row
+		return (solve_recursive(grid, clues, row + 1, 0)); // Move to the next row
+	num = 1; // Start with the number 1
+	while (num <= N) // Loop through possible values
 	{
-		if (is_safe(grid, row, col, num))
+		if (is_safe(grid, row, col, num)) // Check if the current number can be inserted
 		{
-			grid[row][col] = num;
-			if (solve_recursive(grid, clues, row, col + 1))
-				return (1);
-			grid[row][col] = 0;
+			grid[row][col] = num; // Insert the number into the grid
+			if (solve_recursive(grid, clues, row, col + 1)) // Recursive call for the next position
+				return (1); // Return 1 if a solution is found
+			grid[row][col] = 0; // If no solution was found, reset the value
 		}
-		num++;
+		num++; // Move to the next number
 	}
-	return (0);
+	return (0); // Return 0 if no solution was found
 }
 
-void	initialize_grid_and_solve(t_clues clues)
+void	initialize_grid_and_solve(t_clues clues) // Function to initialize the grid and start solving
 {
-	int	grid[N][N];
-	int	i;
-	int	j;
+	int	grid[N][N]; // Declare the grid
+	int	i; // Index for rows
+	int	j; // Index for columns
 
-	i = 0;
-	while (i < N)
+	i = 0; // Initialize the row index
+	while (i < N) // Loop through the rows
 	{
-		j = 0;
-		while (j < N)
+		j = 0; // Initialize the column index
+		while (j < N) // Loop through the columns
 		{
-			grid[i][j] = 0;
-			j++;
+			grid[i][j] = 0; // Initialize the value in the grid
+			j++; // Move to the next column
 		}
-		i++;
+		i++; // Move to the next row
 	}
-	if (solve_recursive(grid, clues, 0, 0))
-		print_matrix(grid);
+	if (solve_recursive(grid, clues, 0, 0)) // Start the recursive solving
+		print_matrix(grid); // If a solution is found, print the matrix
 	else
-		write (1, "No solution found.\n", 20);
+		write (1, "No solution found.\n", 20); // If no solution, print a message
 }
